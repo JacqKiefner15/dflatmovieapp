4d1002c8560d42af812d3209c52ad2af74f13d97
Jacquelyn Kiefner <jacquelyn.kiefner@allata.com>
2021-07-06 09:30:18 -0600

    added project

---
+++ MyApi/Data/CinemaDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MyApi.Models;
+using System.Threading.Tasks;
+
+namespace MyApi.Data
+{
+    public class CinemaDbContext : DbContext
+    {
+        public CinemaDbContext(DbContextOptions<CinemaDbContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
